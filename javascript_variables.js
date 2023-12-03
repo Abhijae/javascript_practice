2914a1ee7f97c7f674e5734efcedaad9b43716ce
Abhijae <abhijae@gmail.com>
2023-12-03 15:49:21 +0530

    initial commit

---
+++ javascript_variables.js
@@ -0,0 +1,17 @@
+let a = 'Sample variables'
+let b = 10
+let c = BigInt('343333')
+let d = BigInt('5433')
+
+
+function sumNumb(b){
+    return b
+}
+
+
+
+value= c + d
+
+data= sumNumb(44)
+console.log(a)
+console.log(data)
