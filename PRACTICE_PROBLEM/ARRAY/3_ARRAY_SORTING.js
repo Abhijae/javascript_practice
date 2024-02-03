377f6f829b37044cc720e4b51cf7fa11f2fbdaa6
Abhijae <abhijae@gmail.com>
2024-02-03 12:52:44 +0530

    needle in a haystack

--- PRACTICE_PROBLEM/ARRAY/3_ARRAY_SORTING.js
+++ PRACTICE_PROBLEM/ARRAY/3_ARRAY_SORTING.js
@@ -1,11 +1,13 @@
-a=[2,3,6,4,5,7]
+a=[2,3,6,4,5,1]
 
 for(i=0;i<a.length;i++){
-    if(a[i]>a[i+1]){
-        x=a[i+1]
-        a[i+1]=a[i]
-        a[i]=x
+    for(j=i+1;j<a.length;j++){
+        if(a[i]>a[j]){
+            x=a[j]
+            a[j]=a[i]
+            a[i]=x
 
+        }
     }
 }
 
