14f0b9c15a0ddabcac0ffba83cf686f4b3fe8f90
Abhijae <abhijae@gmail.com>
2024-07-21 21:47:40 +0300

    sample

---
+++ javascript_4_async_await_syntax.js
@@ -0,0 +1,29 @@
+function promiseFunction(){
+    let p = new Promise(function(resolve){
+        resolve("this is the output from the promise fn")
+    })
+    return p
+}
+
+/*
+function main(){
+    let wb=promiseFunction()
+    wb.then(function(x){
+        console.log(x)
+    })
+}
+*/     
+
+// another way of calling the same fn as async
+
+async function main(){  
+    let wb = await promiseFunction()  // await is only valid in async function
+    console.log(wb)                   // every await needs to be wrapped inside async fn
+}
+
+main()
+
+
+//sample
+
+
